a53bf8937de0875b7d8aa27f12ca417e4eef36e6
magg4152 <hort4152@kettering.edu>
2020-02-14 09:41:31 -0500

    update

--- uLearn/src/Main.java
+++ uLearn/src/Main.java
@@ -1,6 +1,13 @@
 import GUI.GUI;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 
 import javax.swing.*;
+import javax.swing.text.html.CSS;
+import javax.swing.text.html.HTMLEditorKit;
+import java.awt.*;
+
+import static javax.swing.text.html.CSS.Attribute.BACKGROUND_COLOR;
 
 public class Main {
 
@@ -8,11 +15,13 @@ public class Main {
         JFrame frame = new JFrame("uLearn");
         frame.setContentPane(new GUI().Master_Window);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBackground(Color.cyan);
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        frame.setResizable(false);
+        frame.setResizable(true);
 
-        
+
     }
 
 
