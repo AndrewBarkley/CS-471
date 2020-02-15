1cd7fcffe0d85c07545ebe06d99bddf3658acc3f
mag <hort4152@kettering.edu>
2020-02-15 18:42:40 -0500

    Update codeStyleConfig.xml, InitController.class, and 5 more files...

--- uLearnFX/src/ulearn/controllers/InitController.java
+++ uLearnFX/src/ulearn/controllers/InitController.java
@@ -8,7 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
-import javafx.scene.web.WebView;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -17,47 +16,38 @@ import java.io.IOException;
 import static javafx.fxml.FXMLLoader.load;
 
 public class InitController {
-    // for login
-    public Button loginBtn = new Button();
-    public Label unameLabel = new Label(), pwdLabel = new Label();
-    public TextField unameBox = new TextField();
-    public PasswordField pwdBox = new PasswordField();
+	// for login
+	public Button loginBtn = new Button();
+	public Label unameLabel = new Label(), pwdLabel = new Label();
+	public TextField unameBox = new TextField();
+	public PasswordField pwdBox = new PasswordField();
 
+	// for login_popup
+	public Text uNameInfo = new Text(), pwdInfo = new Text(), dispUName = new Text(), dispPwd = new Text();
+	public Button goHomeBtn = new Button();
 
-    // for login_popup
-    public Text uNameInfo = new Text(), pwdInfo = new Text(), dispUName = new Text(), dispPwd = new Text();
-    public Button goHomeBtn = new Button();
+	@FXML private void loginBtnAction(ActionEvent event) throws IOException {
+		event.consume();
+		String path = "../layout/login_popup.fxml";
+		setupOnClick(path, loginBtn, 300, 140, false);
+	}
 
+	@FXML private void mainScreen(ActionEvent event) throws IOException {
+		event.consume();
+		String path = "../layout/main_screen.fxml";
+		setupOnClick(path, goHomeBtn, 600, 350, true);
+	}
 
-    @FXML
-    private void loginBtnAction(ActionEvent event) throws IOException {
-        event.consume();
-        Parent root = FXMLLoader.load(getClass().getResource("../layout/login_popup.fxml"));
-        Stage stage = (Stage) loginBtn.getScene().getWindow();
-        stage.setScene(new Scene(root, 300, 140));
-        stage.setResizable(false);
-        stage.show();
-        resize(stage);
-    }
-
-    @FXML
-    private void mainScreen(ActionEvent event) throws IOException {
-        event.consume();
-        Parent root = FXMLLoader.load(getClass().getResource("../layout/main_screen.fxml"));
-        Stage stage = (Stage) goHomeBtn.getScene().getWindow();
-        Scene scene = new Scene(root, 600, 350);
-        scene.setFill(null);
-        stage.setScene(scene);
-        stage.setResizable(true);
-        stage.show();
-        resize(stage);
-    }
-
-    void resize(Stage stage) {
-        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
-        stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
-        stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 4);
-    }
-
-
+	void setupOnClick(String layout, Button btn, int width, int height, boolean resize) throws IOException {
+		Parent par = FXMLLoader.load(getClass().getResource(layout));
+		Stage stage = (Stage) btn.getScene().getWindow();
+		Scene scene = new Scene(par, width, height);
+		scene.setFill(null);
+		stage.setScene(scene);
+		stage.setResizable(resize);
+		stage.show();
+		Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+		stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
+		stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 4);
+	}
 }
--- uLearnFX/src/ulearn/controllers/MainController.java
+++ uLearnFX/src/ulearn/controllers/MainController.java
@@ -8,34 +8,34 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 public class MainController {
-    // main tabs
-    public TabPane mainTabs;
-    public Tab chatTab = new Tab(), classTab = new Tab(), calTab = new Tab(), storageTab = new Tab(), settingsTab = new Tab();
+	// main tabs
+	public TabPane mainTabs;
+	public Tab chatTab = new Tab(), classTab = new Tab(), calTab = new Tab(), storageTab = new Tab(), settingsTab =
+			new Tab();
 
-    // calendar
-    public WebView gCalWV = new WebView();
-    public WebView driveWView = new WebView();
+	// calendar
+	public WebView gCalWV = new WebView();
+	public WebView driveWView = new WebView();
 
-    @FXML
-    void tabEvents(Event ev) {
-        if (calTab.isSelected()) {
-            gCalendar();
-        } else if (storageTab.isSelected()) {
-            gDrive();
-        }
-    }
+	@FXML void tabEvents(Event ev) {
+		if (calTab.isSelected()) {
+			gCalendar();
+		} else if (storageTab.isSelected()) {
+			gDrive();
+		}
+	}
 
-    void gCalendar() {
-        WebEngine wEngine = gCalWV.getEngine();
-        wEngine.load("https://calendar.google.com");
-        gCalWV.setZoom(0.8);
+	void gCalendar() {
+		WebEngine wEngine = gCalWV.getEngine();
+		wEngine.load("https://calendar.google.com");
+		gCalWV.setZoom(0.8);
 
-    }
+	}
 
-    void gDrive() {
-        WebEngine we = driveWView.getEngine();
-        we.load("https://drive.google.com/drive");
-        driveWView.setZoom(0.8);
-    }
+	void gDrive() {
+		WebEngine we = driveWView.getEngine();
+		we.load("https://drive.google.com/drive");
+		driveWView.setZoom(0.8);
+	}
 
 }
